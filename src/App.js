3e53a0a2d80523437781f712679de5aa4602a85f
rahk1990 <rahkem.ellis@gmail.com>
2022-03-31 11:17:19 -0400

    added functions for fetching chart data.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 // import React, { useState } from 'react';
-
+import SearchBar from "./Components/SearchBar/SearchBar";
+import DisplayEntries from "./Components/DisplayEntries/DisplayEntries";
 import { useState } from "react";
+import axios from "axios"
 
 function App() {
 
   const[entries,setEntries] = useState([{rank:'',name:'',platform:'',year:'',genre:'',publisher:'',northAmericanSales:'',europeSales:'',japanSales:'',otherSales:'',globalSales:''}])
+
+
+  async function getAllGames(){
+    let response = await axios.get('http://127.0.0.1:8000/api/songs/');
+    setEntries(response.data);
+    setSearchEntries(response.data);
+    console.log(response.data)
+  }
+
+  useEffect(() => {
+    getAllGames();
+  }, [])
 
   return (
     <div>
      <h1>
        hello world
      </h1>
+     <div>
+        <SearchBar />
+        <DisplayEntries parentEntries = {entries} />
+     </div>
     </div>
   );
 }
