79ab018cdc876930ea66405bf9ffd9a0f0106902
Nicholas Mccrady <nicholas.mccrady@gmail.com>
2022-04-01 16:26:45 -0500

    made second search button function button displays on page we now just need to get the logic to return the specific entries(global sales

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -8,14 +8,21 @@ const SearchBar = (props) => {
         props.searchGames(searchEntries)
     }
 
+    function globalSales(){
+        
+    }
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <input value={searchEntries} onChange={(e) => setSearchEntries(e.target.value)} type='text' placeholder='Search...'></input>
                 <button type='submit'>Search</button>
+                <button onClick={globalSales}>GlobalSales</button>
+
+                
             </form>
         </div>
     )
+    
 }
 
 export default SearchBar
